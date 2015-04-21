49527859561d1f6212262a78e037c9f0af5719f4
back2me <huta.research@gmail.com>
2015-04-21 16:59:13 +0700

    First Commit

---
+++ src/Project1.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : Project1.cpp
+// Author      : HuTa
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+
+	cout << "****************ABC - <MSSV> ***************" << endl;
+	cout << "** 1. Tinh tien loi ngan hang.           **" << endl;
+	cout << "** 2. Dem so te bao song sot.            **" << endl;
+	cout << "** 3. Gia lap may tinh bo tui don gian.  **" << endl;
+	cout << "** 4. Lich van nien.                     **" << endl;
+	cout << "** 5. Hinh canh quat.                    **" << endl;
+	cout << "********************************************" << endl;
+	_getch();
+	return 0;
+}
